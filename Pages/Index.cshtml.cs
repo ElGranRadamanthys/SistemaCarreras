c0622e00759e38b6402bdae780cdf13861560de7
nicol <nicol@NICO>
2025-07-04 06:34:33 -0300

    Se agrego una imagen en el inicio con una mini explicacion de la pagina

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -13,43 +13,10 @@ namespace SistemaCarreras.Pages
             _logger = logger;
         }
 
-        public List<Carrera> Carreras { get; set; } = new();
-        public List<Alumno> Alumnos { get; set; } = new();
 
         public void OnGet()
         {
-            var carreraUno = new Carrera
-            {
-                Id = 1,
-                Nombre = "Analista de sistemas",
-                Modalidad = "Hibrida",
-                DuracionAnios = 3,
-                TituloOtorgado = "Analista de sistemas"
-            };
-
-            var carreraDos = new Carrera
-            {
-                Id = 2,
-                Nombre = "Analista de sistemas",
-                Modalidad = "Hibrida",
-                DuracionAnios = 3,
-                TituloOtorgado = "Analista de sistemas"
-            };
-
-            var carreraTres = new Carrera
-            {
-                Id = 3,
-                Nombre = "Analista de sistemas",
-                Modalidad = "Hibrida",
-                DuracionAnios = 3,
-                TituloOtorgado = "Analista de sistemas"
-            };
-
-            Carreras.Add(carreraUno);
-            Carreras.Add(carreraDos);
-            Carreras.Add(carreraTres); 
-
-
+            
         }
     }
 }
